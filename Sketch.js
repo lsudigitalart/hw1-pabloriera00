82dbe165b8f735124a27e24d5a07c269a797198f
pabloriera00 <priera1@lsu.edu>
2025-09-03 12:53:30 -0500

    I made a website

---
+++ Sketch.js
@@ -0,0 +1,12 @@
+function Setup (){
+    create canvas (400, 400);
+}
+
+function draw
+    background(200);
+    no stroke();
+    fill(0);
+    ellipse (100, 100, 50, 50);
+    fill (255);
+    ellipse (200, 100, 50, 50);
+}
