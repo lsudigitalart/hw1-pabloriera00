4e278b3fd593beaed5f14bc5497bbed83b80abba
pabloriera00 <priera1@lsu.edu>
2025-09-03 13:48:05 -0500

    Fix function definitions and improve HTML structure

--- Sketch.js
+++ Sketch.js
@@ -1,12 +1,33 @@
-function Setup (){
-    create canvas (400, 400);
+function setup () {
+    createCanvas (400, 400);
 }
 
-function draw
-    background(200);
-    no stroke();
+function draw()  {
+    background(0);
+    noStroke();
+    fill(250);
+    ellipse(200, 180, 100, 100);
+    fill(255);
+    ellipse(200, 100, 50, 50);
+
+    ellipse(200, 310, 150, 150);
+    fill(255);
+
+    ellipse(200, 5, 450, 70);
+    fill(255);
+
+    ellipse(400, 200, 70, 450);
+    fill(255);
+
+    ellipse(15, 200, 70, 470);
+    fill(255);
+
     fill(0);
-    ellipse (100, 100, 50, 50);
-    fill (255);
-    ellipse (200, 100, 50, 50);
+    ellipse(250, 310, 150, 150);
+    
+    fill(0);
+    ellipse(175, 180, 100, 100);
+
+    fill(0);
+    ellipse(215, 100, 50, 50);
 }
