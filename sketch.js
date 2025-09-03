2afc7f7d5998e8a476c6e84aa3adfc61bfb2eb9f
pabloriera00 <priera1@lsu.edu>
2025-09-03 13:53:12 -0500

    Add Sketch.js with initial drawing functions

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup () {
+    createCanvas (400, 400);
+}
+
+function draw()  {
+    background(0);
+    noStroke();
+    fill(250);
+    ellipse(200, 180, 100, 100);
+    fill(255);
+    ellipse(200, 100, 50, 50);
+
+    ellipse(200, 310, 150, 150);
+    fill(255);
+
+    ellipse(200, 5, 450, 70);
+    fill(255);
+
+    ellipse(400, 200, 70, 450);
+    fill(255);
+
+    ellipse(15, 200, 70, 470);
+    fill(255);
+
+    fill(0);
+    ellipse(250, 310, 150, 150);
+    
+    fill(0);
+    ellipse(175, 180, 100, 100);
+
+    fill(0);
+    ellipse(215, 100, 50, 50);
+}
